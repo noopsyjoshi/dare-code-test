9dfcf3310355d1ed1a6232dc1e397166c537c212
noopsyjoshi <noopur.joshi13@gmail.com>
2020-10-04 19:27:48 +0100

    added navigation

--- dare-code-test/src/components/Navigation.vue
+++ dare-code-test/src/components/Navigation.vue
@@ -1,14 +1,53 @@
 <template>
   <nav class="navigation">
-    <div class="navigation__toggle"><span>Menu</span></div>
+    <div class="navigation__toggle">
+      <div
+        id="open"
+        class="navigation__toggle-btn navigation__toggle-btn--open"
+      >
+        <span>Menu</span>
+      </div>
+      <div
+        id="close"
+        class="navigation__toggle-btn navigation__toggle-btn--close"
+      >
+        <span>Close</span>
+      </div>
+    </div>
     <div class="navigation__menu">
       <ul class="navigation__list">
-        <li><router-link to="/blusher">Blusher</router-link></li>
-        <li><router-link to="/brushes">Brushes</router-link></li>
-        <li><router-link to="/mascara">Mascara</router-link></li>
-        <li><router-link to="/perfume">Perfume</router-link></li>
-        <li><router-link to="/hair">Hair</router-link></li>
+        <li>
+          <router-link class="navigation__link" to="/blusher"
+            >Blusher</router-link
+          >
+        </li>
+        <li>
+          <router-link class="navigation__link" to="/brushes"
+            >Brushes</router-link
+          >
+        </li>
+        <li>
+          <router-link class="navigation__link" to="/mascara"
+            >Mascara</router-link
+          >
+        </li>
+        <li>
+          <router-link class="navigation__link" to="/perfume"
+            >Perfume</router-link
+          >
+        </li>
+        <li>
+          <router-link class="navigation__link" to="/hair">Hair</router-link>
+        </li>
       </ul>
     </div>
   </nav>
 </template>
+
+<style lang="scss">
+@import "@/assets/scss/components/_navigation.scss";
+</style>
+
+<script>
+export default {};
+</script>
--- dare-code-test/src/main.js
+++ dare-code-test/src/main.js
@@ -1,10 +1,22 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import Navigation from "./utils/navigation.js";
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+
+  created() {},
+
+  mounted() {
+    Navigation();
+  },
+
+  destroyed() {},
+
+  updated() {},
+
   render: h => h(App)
 }).$mount("#app");
